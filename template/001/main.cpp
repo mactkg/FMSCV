5f93f8d3a3b1dcda41f645361deafc59dbc531f0
mactkg <mactkg@gmail.com>
2015-10-14 18:02:12 +0900

    init

---
+++ template/001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  template 
+//
+//  Created by Kenta Hara on 2015/10/14.
+//  Copyright © 2015年 Kenta Hara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
